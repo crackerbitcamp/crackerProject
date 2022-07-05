dd81e655f74c5fb32b54b49079c6c636c157de07
soyoon <soyoon@gmail.com>
2022-07-05 15:37:14 +0900

    Admin Package 추가

--- crackerProject/src/main/java/admin/controller/AdminController.java
+++ crackerProject/src/main/java/admin/controller/AdminController.java
@@ -2,7 +2,9 @@ package admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping(value="admin")
@@ -32,4 +34,8 @@ public class AdminController {
 		return mav;
 		
 	}
+	
+
+	
+	
 }
---
+++ crackerProject/src/main/java/admin/dao/AdminDAOMyBatis.java
@@ -0,0 +1,15 @@
+package admin.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class AdminDAOMyBatis implements AdminDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+}
