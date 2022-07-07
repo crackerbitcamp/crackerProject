d771a3defd008c15904f920dc92701af7d155463
jo <luckghost123@naver.com>
2022-07-07 18:20:21 +0900

    문의하기 수정, sql조건 변경

--- crackerProject/src/main/java/customer/bean/CustomerDTO.java
+++ crackerProject/src/main/java/customer/bean/CustomerDTO.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public class CustomerDTO {
 	private int seq;
 	private String id;
+	private String name;
 	private String email;
 	private String subject;
 	private String content;
@@ -14,6 +15,13 @@ public class CustomerDTO {
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
 	Date logtime;
 
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 	public int getSeq() {
 		return seq;
 	}
